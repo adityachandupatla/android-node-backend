031fb6f3863c89f906dfc4342feb0cdbf084df1d
Aditya Ch <ch.aditya1996@gmail.com>
2020-11-17 00:55:41 -0800

    Bug fixes in api.js

--- api.js
+++ api.js
@@ -9,23 +9,20 @@ function fetchData(baseurl, params, parser) {
             if (apiRes.status === 200) {
                 return apiRes.json()
             } else {
-                let message = 'GET request ' + url + ' failed. Reason: ' + apiRes.statusText
+                let message = 'GET request ' + baseurl + ' failed. Reason: ' + apiRes.statusText
                 throw { 'status': 500, 'message': message }
             }
         })
-        .then(apiJson => {
-            let parsedRes = parser(apiJson)
+        .then(apiJson => parser(apiJson))
+        .then(parsedRes => {
             if (!parsedRes) {
-                let message = 'Parsing of the response from: ' + url + ' failed.'
+                let message = 'Parsing of the response from: ' + baseurl + ' failed.'
                 throw { 'status': 500, 'message': message }
             } else {
                 return { 'status': 200, 'message': parsedRes }
             }
         })
-        .catch(err => {
-            console.log(err)
-            return err
-        })
+        .catch(err => { return err })
 }
 
 module.exports = {
